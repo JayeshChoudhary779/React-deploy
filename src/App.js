04770ee00b3f893e8f24fd001ea03c873a3be5e4
JayeshChoudhary779 <82047882+JayeshChoudhary779@users.noreply.github.com>
2021-05-26 00:19:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Heading from "./Heading"
+import ChildDiv from "./ChildDiv";
+import Calculator from "./calculator";
 function App() {
   return (
      <>
